a11a031a52b1b99a4bf2cf029b53b8000a926dc1
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-02-18 18:02:55 +0000

    Added

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Joanna Keza
+# Date: February 28, 2025
+# This code calculates the area and perimeter of a 2cm X 10cm rectangle.
+
+
+def main():
+    print ("For a rectangle with length 2cm and width 10cm.")
+    print ()
+    print ("The area is:  {}cm^2".format (2 * 10))
+    print ("The perimeter is:  {}cm".format (2 * (2 + 10)))
+
+
+if __name__ == "__main__":
+    main()
